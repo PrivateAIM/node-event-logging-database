4e442f7cd66064fd339765c427500043d80f1bc6
pbrassel <52356233+pbrassel@users.noreply.github.com>
2026-01-08 15:39:12 +0100

    docs: `EventLog` docstring

--- node_event_logging/crud.py
+++ node_event_logging/crud.py
@@ -14,6 +14,31 @@ class BaseModel(pw.Model):
 class EventLog(BaseModel):
     # Schema is derived from OpenTelemetry's event definition.
     # https://opentelemetry.io/docs/specs/otel/logs/data-model/#log-and-event-record-definition
+    """Database table schema for event logs.
+
+    Attributes
+    ----------
+    event_name : str
+        A name that identifies a certain type of events. This name should uniquely identify the event structure (both
+        body and attributes). This attribute is not nullable. This column is indexed to
+        improve query performance.
+    service_name : str
+        The name of the service that creates the event log. This is attribute is not nullable. This column is indexed to
+        improve query performance.
+    timestamp : datetime.datetime
+        The exact timestamp when the event took place. Defaults to the point in time when the event log is created. This
+        column is indexed to improve query performance.
+    body : str
+        A string containing the body of the event log, i.e. a human-readable string message (including multi-line). This
+        attribute is nullable.
+    attributes : dict
+        Additional information about the event stored in a structured way so that they can be accessed easily by the UI.
+        This attribute is nullable.
+
+    Notes
+    -----
+    An id column is automatically created by peewee.
+    """
 
     event_name = pw.CharField(index=True)
     service_name = pw.CharField(index=True)
