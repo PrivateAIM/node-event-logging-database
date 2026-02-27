75f6a2ef9d1a6d6674447a42ceb1b57867dd1c28
pbrassel <52356233+pbrassel@users.noreply.github.com>
2026-02-27 15:58:50 +0100

    feat: use a database proxy and provide an initialization function

--- node_event_logging/crud.py
+++ node_event_logging/crud.py
@@ -1,4 +1,3 @@
-from contextlib import contextmanager
 import datetime
 
 import peewee as pw
@@ -8,8 +7,12 @@ from playhouse.shortcuts import ThreadSafeDatabaseMetadata
 from .validation import EventModelMap
 
 
+proxy = pw.DatabaseProxy()
+
+
 class BaseModel(pw.Model):
     class Meta:
+        database = proxy
         model_metadata_class = ThreadSafeDatabaseMetadata
 
 
@@ -64,9 +67,9 @@ class EventLog(BaseModel):
         super().create(**query)
 
 
-@contextmanager
-def bind_to(db: pw.Database):
-    with db.bind_ctx((EventLog,)):
+def init_db(db: pw.Database):
+    """Initializes a configured database with the help of the database proxy that is already bound to the models."""
+    proxy.initialize(db)
+    with db:
         # Create tables if they do not exist yet.
         db.create_tables((EventLog,))
-        yield
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,18 @@
 import peewee as pw
 import pytest
 
+from node_event_logging import init_db
 from tests.common.env import postgres_user, postgres_database, postgres_password, postgres_host, postgres_port
 
 
 @pytest.fixture(scope="package")
 def postgres():
-    return pw.PostgresqlDatabase(
+    db = pw.PostgresqlDatabase(
         postgres_database(),
         user=postgres_user(),
         password=postgres_password(),
         host=postgres_host(),
         port=postgres_port(),
     )
+    init_db(db)
+    return db
